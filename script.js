7606552e979ec61784f01285fc52b988bfeef931
Joao Nkunku <joaonkunku01@gmail.com>
2025-08-19 17:27:52 +0200

    Fixing code

--- script.js
+++ script.js
@@ -1,60 +1,40 @@
-    //variables
+//variables
+const calcualteButton = document.getElementById("calculateBtn"); 
+const textResult = document.getElementById("textResult");
 
-    const calcualteButton = document.getElementById("calculateBtn"); 
-    const textResult = document.getElementById("textResult");
-    const result = document.getElementById("result");
+calcualteButton.addEventListener("click", () => {
+    const courseName = document.getElementById("CourseName").value.trim();
+    const assignmentScore = document.getElementById("assigScore").value;
+    const assignmentWeight = document.getElementById("assigWeight").value;
 
-    calcualteButton.addEventListener("click",()=>{
+    const courseDetail = {
+        course: courseName,
+        score: parseFloat(assignmentScore),
+        weight: parseFloat(assignmentWeight),
 
-        const courseName = document.getElementById("CourseName").value.trim()
-        const assignmentScore = document.getElementById("assigScore").value
-        const assignmentWeight = document.getElementById("assigWeight").value
-
-
-        showConatiner()
-
-        const courseDetail = {
-            course: courseName,
-            score: assignmentScore,
-            weight: assignmentWeight,
-
-            determineGrade: function(){
-                // formula to determine the grade
-                const finalGrade = this.score * this.weight
-                return finalGrade;
-            },
-            
-            // displaying the grade to the student
-            displayGrade: function(){
-                const grade = this.determineGrade();
-
-                if(grade > 79){
-                    textResult.innerText = "You Achieved An:"
-                        result.innerText = "A"
-                }else if(grade <= 80 && grade >= 70){
-                    textResult.innerText = "You Achieved An:"
-                        result.innerText = "B"
-
-                }else if(grade <= 70 && grade >= 60){
-                    textResult.innerText = "You Achieved An:"
-                        result.innerText = "C"
-                }else if(grade <= 60 && grade >=50){
-                    textResult.innerText = "You Achieved An:"
-                        result.innerText = "D"
-                }else{
-                    textResult.innerText = "You Achieved An:"
-                        result.innerText = "F"
-                }
+        //trying to validate so i can prevent incorrect values 
+        validation:function() {
+            if (!this.course) {
+                textResult.innerText = "Please fill in the input";
+                return false;
             }
-        }
+        },
 
-        courseDetail.displayGrade()
-    })
-
-
-    function showConatiner(){
-        const resultContainer = document.querySelector(".resultContainer");
-        resultContainer.style.display = "flex"
+        determineGrade: function() {
+            // formula to determine the grade
+            
+        },
         
-        }
+        
+    };
+
+    
+});
+
+function showConatiner() {
+    const resultContainer = document.querySelector(".resultContainer");
+    resultContainer.style.display = "flex";
+}
+
+
 
