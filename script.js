8203fc91f86a11f0fe941e0a68b767ab7df627fa
Joao Nkunku <joaonkunku01@gmail.com>
2025-08-19 18:16:24 +0200

    finally works

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //variables
-const calcualteButton = document.getElementById("calculateBtn"); 
+const calculateButton = document.getElementById("calculateBtn"); 
 const textResult = document.getElementById("textResult");
 
-calcualteButton.addEventListener("click", () => {
+calculateButton.addEventListener("click", () => {
     const courseName = document.getElementById("CourseName").value.trim();
     const assignmentScore = document.getElementById("assigScore").value;
     const assignmentWeight = document.getElementById("assigWeight").value;
@@ -13,25 +13,41 @@ calcualteButton.addEventListener("click", () => {
         weight: parseFloat(assignmentWeight),
 
         //trying to validate so i can prevent incorrect values 
-        validation:function() {
-            if (!this.course) {
-                textResult.innerText = "Please fill in the input";
+        validation:function(){
+
+            if(!this.course||this.score === " " ||this.weight === " "){
+                textResult.innerText = "You cant leave the input-felid empty"
                 return false;
+            }else{
+                courseDetail.performCalculation()
             }
         },
 
-        determineGrade: function() {
-            // formula to determine the grade
-            
-        },
+        performCalculation:function(){
+            const grade = this.score * (this.weight/100);
+
+            if(grade >= 80){
+                textResult.innerText = "You Achieved An: A"
+            }else if(grade >= 70 && grade <= 80){
+                textResult.innerText = "You Achieved An: B"
+            }else if(grade >=60 && grade <= 70){
+                textResult.innerText = "You Achieved An: C"
+            }else if(grade >= 50 && grade <= 60){
+                textResult.innerText = "You Achieved An: E"
+            }else{
+                textResult.innerText = "You Achieved An: F"
+            }
+
+        }
         
         
     };
+    showContainer()
+    courseDetail.validation()
 
-    
 });
 
-function showConatiner() {
+function showContainer() {
     const resultContainer = document.querySelector(".resultContainer");
     resultContainer.style.display = "flex";
 }
